fe39a7979b24ae3e01891b2e28dec35341ea5ac5
Collin Smith <cmsmith.tx@gmail.com>
2019-11-11 11:11:51 -0600

    adding thunk

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -6,3 +6,13 @@ export const addArticle = (payload) => {
     payload
   }
 };
+
+export const getData = () => {
+  return function(dispatch){
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        dispatch({ type: 'DATA_LOADED', payload: json });
+      });
+  };
+}
