9faa7d7241360d601f3b1f5f99b534c28a1eaa96
Collin Smith <cmsmith.tx@gmail.com>
2019-11-11 11:28:11 -0600

    bringing in api results

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import List from './List';
 import Form from './Form';
+import Post from './Posts';
 import '../../../src/App.css';
 
 const App = () => (
@@ -12,6 +13,10 @@ const App = () => (
     <div className="add-article">
         <h2>Add a new article</h2>
         <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
     </div>
   </div>
 );
